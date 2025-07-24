eeaa7c0d8eee2fb9ae785b466b5ddcef56d6d0c8
Lucas Romao <luckh049@gmail.com>
2025-07-24 03:18:11 -0300

    Naming adjustment

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from config import destino_csv
+from config import csv_path
 from unzipDataBase import unzip_and_move_database
 from downloadDataBase import download_database
 
 def main():
     download_database()
-    unzip_and_move_database(destino_csv)
+    unzip_and_move_database(csv_path)
 
 if __name__ == "__main__":
     main()
--- unzipDataBase.py
+++ unzipDataBase.py
@@ -2,8 +2,7 @@ import os
 import gzip
 import shutil
 
-
-def unzip_and_move_database(destino_csv):
+def unzip_and_move_database(csv_path):
     arquivo_gz = os.path.join(os.path.expanduser("~"), "Downloads", "world_imdb_movies_top_movies_per_year.csv.gz")
 
     # Verifica se o arquivo compactado existe
@@ -12,10 +11,10 @@ def unzip_and_move_database(destino_csv):
         exit()
 
     # Apaga arquivo CSV antigo, se existir
-    if os.path.exists(destino_csv):
+    if os.path.exists(csv_path):
         try:
-            os.remove(destino_csv)
-            print(f"Arquivo antigo removido: {destino_csv}")
+            os.remove(csv_path)
+            print(f"Arquivo antigo removido: {csv_path}")
         except Exception as e:
             print(f"Erro ao apagar arquivo antigo: {e}")
             exit()
@@ -23,9 +22,9 @@ def unzip_and_move_database(destino_csv):
     try:
         # Descompacta o .gz e salva o .csv
         with gzip.open(arquivo_gz, 'rb') as arquivo_compactado:
-            with open(destino_csv, 'wb') as arquivo_csv:
+            with open(csv_path, 'wb') as arquivo_csv:
                 shutil.copyfileobj(arquivo_compactado, arquivo_csv)
-        print(f"Arquivo descompactado com sucesso em:\n{destino_csv}")
+        print(f"Arquivo descompactado com sucesso em:\n{csv_path}")
         
         # Remove o arquivo .gz
         os.remove(arquivo_gz)
